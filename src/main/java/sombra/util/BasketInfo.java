53f517f61046338cc1b0ca2ab8720e9b9677710c
DrBAXA <drbaxa@hotmail.com>
2015-01-28 23:37:09 +0200

    basket as map

--- src/main/java/sombra/util/BasketInfo.java
+++ src/main/java/sombra/util/BasketInfo.java
@@ -2,33 +2,37 @@ package sombra.util;
 
 import sombra.entity.Article;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BasketInfo {
-    private List<Integer> articles = new LinkedList<>();
+    private Map<Integer, Integer> articles = new HashMap<>();
     private int price;
 
     public BasketInfo() {
     }
 
-    public BasketInfo(List<Article> basket) {
-        for(Article article : basket){
-            articles.add(article.getId());
-            price += article.getPrice();
+    public BasketInfo(Map<Article, Integer> basket) {
+        for(Article article : basket.keySet()){
+            articles.put(article.getId(), basket.get(article));
+            price += article.getPrice()*basket.get(article);
         }
     }
 
     public void addToBasket(Article article){
-        articles.add(article.getId());
+        if(articles.containsKey(article.getId())){
+            articles.put(article.getId(), articles.get(article.getId())+1);
+        } else {
+            articles.put(article.getId(), 1);
+        }
         price += article.getPrice();
     }
 
-    public List<Integer> getArticles() {
+    public Map<Integer, Integer> getArticles() {
         return articles;
     }
 
-    public void setArticles(List<Integer> articles) {
+    public void setArticles(Map<Integer, Integer> articles) {
         this.articles = articles;
     }
 
